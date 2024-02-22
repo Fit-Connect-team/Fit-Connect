e80c65c64ae4670db731efe0be43c4f9c9238f9e
Idrissjem <idriss.jemli@esprit.tn>
2024-02-22 15:14:40 +0100

    Your commit message here

---
+++ src/Form/User1Type.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class User1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('password',RepeatedType::class, [
+                'type'=>PasswordType::class,
+                'first_options'=>['label'=>'Password'],
+                'second_options'=>['label'=>'Confirm Password']
+            ])
+            ->add('name')
+            ->add('prenom')
+            ->add('tel')
+            ->add('image', FileType::class, [
+                'label' => 'Votre Image (JPG, JPEG, PNG file)',
+                'mapped' => false,
+                'required' => false,
+                'attr' => ['accept' => 'image/*'],
+            ])
+           
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
